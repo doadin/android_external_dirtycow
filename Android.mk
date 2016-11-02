1931fe1af14c20a8b0576e6bcb36a8a4dc0edcb9
sirmordred <bambu_bambu@windowslive.com>
2016-11-02 04:01:25 +0200

    Rootter

--- Android.mk
+++ Android.mk
@@ -10,35 +10,17 @@ LOCAL_SDK_VERSION := 21
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := cow-run-as
-LOCAL_MODULE_FILE := run-as
+LOCAL_MODULE := root-app_process64
 LOCAL_SRC_FILES := \
-	run-as.c
-LOCAL_SDK_VERSION := 21
-include $(BUILD_EXECUTABLE)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := recowvery-applypatch
-LOCAL_SRC_FILES := \
-	recowvery-applypatch.c
-LOCAL_C_INCLUDES := external/libbootimg
-LOCAL_CFLAGS += -DDEBUG -Os
-LOCAL_SHARED_LIBRARIES := liblog
-LOCAL_STATIC_LIBRARIES := libbootimg-static
-include $(BUILD_EXECUTABLE)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := recowvery-app_process64
-LOCAL_SRC_FILES := \
-	recowvery-app_process64.c
+	root-app_process64.c
 LOCAL_CFLAGS += -DDEBUG -Os
 LOCAL_SHARED_LIBRARIES := liblog libcutils libselinux
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := recowvery-run-as
+LOCAL_MODULE := root-run-as
 LOCAL_SRC_FILES := \
-	recowvery-run-as.c
+	root-run-as.c
 LOCAL_CFLAGS += -Os
 LOCAL_SHARED_LIBRARIES := libselinux
 include $(BUILD_EXECUTABLE)
